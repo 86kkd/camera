df5c94e0c1bc4f681598a17aa0ab034005c85cbd
86kkd <2973720373@qq.com>
2024-03-31 17:38:58 +0800

    fix floodfile bug

--- enhance_image.py
+++ enhance_image.py
@@ -3,34 +3,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from utils import *
 import os     
-# Read the image
-background_path = './image_328'
-img_path = './image_before'
+
+fail_backgroud = []
+def get_enahnced_img(img,bg_list):
+
+    for background in np.random.choice(bg_list, 10, replace=False):
+        bg_img = cv2.imread(background)
+        try:
+            enhanced = enhance_image(img,bg_img)
+        except :
+            print("Error can't find image in background")
+            fail_backgroud.append(background)
+            # TODO check if deleted
+            del background
+            return None
+    return enhanced
 
 def read_background(path):
     sample_list = []
     
     for root,dir,files in os.walk(path):
         for file in files :
-            extend_name = ['.jpg','.png']
+            extend_name = ('.jpg','.png')
             if file.endswith(extend_name):
                 sample_list.append(os.path.join(root,file))
 
     return sample_list
 
+# Read the image
+background_path = './image_328'
+img_path = './image_before'
 bg_list = read_background(background_path)
-
-def get_enahnced_img(bg_list):
-    
-    for background in np.random.choice(bg_list, 10, replace=False):
-        bg_img = cv2.imread(background)
-        enhanced = enhance_image(img,bg_img)
-    return enhanced
 
 for root,dir,files in os.walk(img_path):
     for file in files:
-        extend_name = ['.jpg','.png']
+        extend_name = ('.jpg','.png')
         if file.endswith(extend_name):
             file_calss = root.split('/')[-1]
             file = os.path.join(root,file)
             img = cv2.imread(file)
+            enhanced = get_enahnced_img(img,bg_list)
+            if enhanced is None:
+                continue
+            plt.imshow(cv2.cvtColor(enhanced, cv2.COLOR_BGR2RGB))
+            plt.show()
--- utils.py
+++ utils.py
@@ -23,34 +23,41 @@ def get_approx_angle(approx) -> list[np.array,np.array]:
   
   return center, angle
   
-def enhance_image(background,img):
+def enhance_image(img,background):
   # floodfill algorithm
   mask = np.zeros(np.add(background.shape,2)[:2], np.uint8)
-  cv2.floodFill(background.copy(), mask, (0,0), 255, (30,30,30), (100,100,100), cv2.FLOODFILL_FIXED_RANGE)
-  contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-  # cv2.drawContours(mask, contours, -1, 255, 3)
+  flood_img = background.copy()
+  flood_deta = (10,10,10)
+  cv2.floodFill(flood_img, mask, (0,0), 255, flood_deta, flood_deta)
+  contours, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+  cv2.drawContours(mask, contours, -1, 255, 3)
   center,angle = None,None
   approx_size = None
 
+  iter_num = 0
   for contour in contours:
-      # calculate perimeter
-      perimeter = cv2.arcLength(contour, True)
-      approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+    # calculate perimeter
+    perimeter = cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
 
-      # get the position and angle of image
-      if len(approx) == 4 and cv2.isContourConvex(approx) and cv2.contourArea(approx) > 1000:
-          cv2.drawContours(mask, [approx], -1, 255, 3)
-          center, angle = get_approx_angle(approx)
+    # get the position and angle of image
+    if len(approx) == 4 and cv2.isContourConvex(approx) and cv2.contourArea(approx) > 1000 and cv2.contourArea(approx) < 10000:
+      cv2.drawContours(mask, [approx], -1, 255, 3)
+      center, angle = get_approx_angle(approx)
 
-          approx = np.squeeze(approx)
-          approx_up_width = np.sqrt(np.dot(approx[0]-approx[1],approx[0]-approx[1])) 
-          approx_left_length = np.sqrt(np.dot(approx[1]-approx[2],approx[1]-approx[2]))
-          approx_down_width = np.sqrt(np.dot(approx[2]-approx[3],approx[2]-approx[3]))
-          approx_right_length = np.sqrt(np.dot(approx[3]-approx[0],approx[3]-approx[0]))
-
-      # calculate the image size in the realworld snapshot image
+      approx = np.squeeze(approx)
+      approx_up_width = np.sqrt(np.dot(approx[0]-approx[1],approx[0]-approx[1])) 
+      approx_left_length = np.sqrt(np.dot(approx[1]-approx[2],approx[1]-approx[2]))
+      approx_down_width = np.sqrt(np.dot(approx[2]-approx[3],approx[2]-approx[3]))
+      approx_right_length = np.sqrt(np.dot(approx[3]-approx[0],approx[3]-approx[0]))
       approx_size = np.multiply([(approx_up_width + approx_down_width),(approx_left_length + approx_right_length)],0.6)
+      break
+    iter_num += 1
+  assert iter_num <= len(contours), "No quadrilateral found in the image"
+  # calculate the image size in the realworld snapshot image
 
+  mask = np.zeros(np.add(background.shape,2)[:2], np.uint8)
+  cv2.drawContours(mask, [approx], -1, 255, 3)
   # caltulate resize ratio
   max_side_length = approx_size.max()
   img_side_length = img.shape[0]
