074015556b02a8ca52fd04a44fbffc059c711e67
86kkd <2973720373@qq.com>
2024-05-01 21:30:58 +0800

    updage method to load data

--- eval_tflite.py
+++ eval_tflite.py
@@ -10,39 +10,41 @@ from tqdm import tqdm
 import argparse
 
 parser = argparse.ArgumentParser(description="eval tflite model")
-parser.add_argument('--lite',default='/tmp/tflite_model',metavar='str',
+parser.add_argument('--lite',default='/tmp/tflite_model/out_put.tflite',metavar='str',
                     help='path to tflite model')
 parser.add_argument('--val',default='data/valiation',metavar='str',
                     help='path to valiation data')
+parser.add_argument('--image-size',default=224,metavar='N',
+                    help="size of image to feed model")
+parser.add_argument('--cls-file',default='class.txt',metavar='str',
+                    help='class type file')
+
 args = parser.parse_args()
-# 构建数据集
-def load_image(path):
-    """加载一个图像并将其转换为张量"""
-    img = tf.io.read_file(path)
-    img = tf.image.decode_jpeg(img, channels=3)
-    img = tf.image.resize(img, [224, 224])
-    # img = tf.transpose(img, [2, 0, 1])  # 重排维度为 (batch, channels, height, width)
-    return img
 
-def preprocess_image(img, size=(224, 224)):
-    """对图像进行预处理，例如调整大小和归一化"""
-    img = tf.image.resize(img, size)
-    img = tf.cast(img, tf.float32)
-    
-    img /= 255.0
-    IMAGENET_DEFAULT_MEAN = tf.constant([0.485, 0.456, 0.406])
-    IMAGENET_DEFAULT_STD = tf.constant([0.229, 0.224, 0.225])
-    mean_array = tf.expand_dims(IMAGENET_DEFAULT_MEAN, axis=0)
-    std_array = tf.expand_dims(IMAGENET_DEFAULT_STD, axis=0)
-    img = (img - mean_array) / std_array
-
-    return img
-data_directory = args.val
-# 使用 tf.data 从目录构建数据集
-dataset = tf.data.Dataset.list_files(f'{data_directory}/*/*.jpg')  # 根据你的文件格式调整
-dataset = dataset.map(load_image)
-dataset = dataset.map(lambda img: (preprocess_image(img), img))
-dataset = dataset.batch(1)  # 一次性处理 1 张图像
+if args.cls_file:
+    with open(args.cls_file, 'r') as file:
+        class_names = file.read().splitlines()
+else:
+    class_names = None
+val_data = tf.keras.preprocessing.image_dataset_from_directory(
+    args.val,
+    labels='inferred',
+    label_mode='int',
+    class_names=class_names,
+    color_mode='rgb',
+    batch_size=1,
+    image_size=(args.image_size, args.image_size),
+    shuffle=True,
+    seed=1,
+    # validation_split=1.0,
+    # subset='validation',
+    interpolation='bilinear',
+    follow_links=True,
+    crop_to_aspect_ratio=False,
+    # pad_to_aspect_ratio=False,  # 填充（pad）到一个正方形
+    # data_format=None,  # channel_first or chalnnel_last
+    # verbose=True
+)
 
 interpreter = tf.lite.Interpreter(model_path=args.lite)
 # Get input and output tensors.
@@ -54,9 +56,9 @@ print(input_details)
 print(output_details)
 
 # 对数据集进行推理
-for batch, (images, display_images) in enumerate(dataset):
+for batch, (images, lable) in enumerate(val_data):
     # 由于现在每次处理一张图片，所以不需要 take(1) 限制和 batch 条件判断
-
+    display_image = images
     input_type = input_details[0]['dtype']
     if input_type == np.int8:
         input_scale, input_zero_point = input_details[0]['quantization']
@@ -67,21 +69,19 @@ for batch, (images, display_images) in enumerate(dataset):
         images = tf.cast(images,input_details[0]["dtype"])
 
     interpreter.set_tensor(input_details[0]['index'], images) 
-
     interpreter.invoke()
-
     output_data = interpreter.get_tensor(output_details[0]['index'])
 
     # 获取最可能的类别标签的索引
     predicted_label = tf.argmax(tf.squeeze(output_data), axis=0).numpy() # 使用 .numpy() 来获取 Python 整数
 
-
     # 显示图像和对应的预测标签
     fig, axs = plt.subplots(1, 1, figsize=(20, 2))
     # display_images = tf.transpose(display_images, perm=[0, 3, 1, 2 ])
-    img = (tf.squeeze(display_images).numpy()).astype('uint8')  # 转换回 uint8 类型
+    img = (tf.squeeze(display_image).numpy()).astype('uint8')  # 转换回 uint8 类型
     axs.imshow(img)
-    axs.set_title(f'Label: {predicted_label} ')
+    axs.set_title(f'predicted: {predicted_label} label:{lable}')
+    print(f'the shape of output_data:{output_data}')
     print(f'Predicted label: {predicted_label}')
     axs.axis('off')
     plt.show()
